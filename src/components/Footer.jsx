0047e4ab0b546355d6a7317092d072696606ea57
Ole M. Røsand <77734811+omrosand@users.noreply.github.com>
2023-05-29 23:27:26 +0200

    fixing some scss

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,16 +3,18 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer>
-      <section>
-        <h4>Kontakt:</h4>
-        <p>rosandolemartin@gmail.com</p>
-      </section>
-      <section>
-        <h4>Lenker:</h4>
-        <Link to="https://github.com/omrosand" target="_blank">
-          GitHub
-        </Link>
-        <p>LinkedIn</p>
+      <section className="footerContent">
+        <section>
+          <h4>Kontakt:</h4>
+          <p>rosandolemartin@gmail.com</p>
+        </section>
+        <section>
+          <h4>Lenker:</h4>
+          <Link to="https://github.com/omrosand" target="_blank">
+            GitHub
+          </Link>
+          <p>LinkedIn</p>
+        </section>
       </section>
     </footer>
   );
